236a8a467dbd75691e9ccac9cc0bd86e36e8a7b5
lovis-coffee <erikatangkas0701@gmail.com>
2025-04-29 15:27:41 +0700

    Update menu-data.js

--- menu-data.js
+++ menu-data.js
@@ -15,7 +15,7 @@ const MenuData = (function() {
      */
     function initialize() {
         // Fetch the CSV file
-        fetch('data/menu_lovis.csv')
+        fetch('menulovis.csv')
             .then(response => {
                 if (!response.ok) {
                     throw new Error(`Failed to load menu data: ${response.status}`);
